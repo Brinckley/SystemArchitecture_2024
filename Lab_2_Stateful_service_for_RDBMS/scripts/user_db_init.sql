e506f2084b0befeb045d506c0d6982c3231fc7dd
Aleksandr Savin <57827932+Brinckley@users.noreply.github.com>
2024-03-17 21:50:54 +0300

    db + devcontainers wip

--- Lab_2_Stateful_service_for_RDBMS/scripts/user_db_init.sql
+++ Lab_2_Stateful_service_for_RDBMS/scripts/user_db_init.sql
@@ -1,10 +1,26 @@
 CREATE SCHEMA IF NOT EXISTS social_network;
 
 CREATE TABLE IF NOT EXISTS social_network.account (
-  id             SERIAL PRIMARY KEY, 
-  username       VARCHAR (50) UNIQUE NOT NULL, 
-  password       VARCHAR (50) NOT NULL, 
-  first_name     VARCHAR (255) NOT NULL, 
-  last_name      VARCHAR (255) NOT NULL, 
-  email          VARCHAR (255) UNIQUE NOT NULL
+  id            SERIAL PRIMARY KEY, 
+  username      VARCHAR (50) UNIQUE NOT NULL, 
+  password      VARCHAR (50) NOT NULL, 
+  first_name    VARCHAR (255) NOT NULL, 
+  last_name     VARCHAR (255) NOT NULL, 
+  email         VARCHAR (255) UNIQUE NOT NULL
+);
+
+CREATE TABLE social_network.post (
+  id            SERIAL PRIMARY KEY,
+  user_id       INT NOT NULL,
+  content       TEXT NOT NULL,
+  CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES social_network.account(id)
+);
+
+CREATE TABLE social_network.message (
+  id            SERIAL PRIMARY KEY,
+  sender_id     INT NOT NULL,
+  receiver_id   INT NOT NULL,
+  content       TEXT NOT NULL,
+  CONSTRAINT fk_sender   FOREIGN KEY(user_id) REFERENCES social_network.account(id),
+  CONSTRAINT fk_reciever FOREIGN KEY(user_id) REFERENCES social_network.account(id)
 );
